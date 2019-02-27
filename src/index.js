43758346154b692defe44bb43cc8c28bc84dffa5
shapurid <i.v.korney@yandex.ru>
2019-02-28 00:08:08 +0300

    fourth step fix1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import readlineSync from 'readline-sync';
 
+export const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 export const brainEven = () => {
-  const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
   console.log('Answer "yes" if number even otherwise answer "no".\n');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}!\n`);
-  for (let i = 1; i <= 3; i += 1) {
+  const numberOfQuestions = 3;
+  for (let i = 1; i <= numberOfQuestions; i += 1) {
     const question = randomNumber(1, 99);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
     console.log(`Question: ${question}`);
     const answerOfUser = readlineSync.question('Answer: ');
-    if (question % 2 === 0 && answerOfUser !== 'yes') {
-      console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${username}!`);
-      break;
-    } else if (question % 2 !== 0 && answerOfUser !== 'no') {
-      console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${username}!`);
-      break;
-    } else {
+    if (answerOfUser === correctAnswer) {
       console.log('Correct!');
-      if (i === 3) {
-        console.log(`Congratulations, ${username}!`);
-      }
+    } else {
+      console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${username}!`);
+      return;
     }
   }
+  console.log(`Congratulations, ${username}!`);
 };
-
 
 export const greet = () => {
   console.log('Welcome to the Brain Games!\n');
